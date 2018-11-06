706852884b5092edeb729ceda0916012946ea909
Cheng Vue <cvu3@ucsc.edu>
2018-11-06 19:28:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,18 +32,4 @@ function currentLine(lineArr) {
   }
 
   return 'The line is currently: ' + customersInLineStr;
-=======
-function takeANumber(arr, name) {
-  arr.push(name);
-  var numberInLine = arr.length;
-  return `Welcome, ${name}. You are number ${numberInLine} in line.`
-}
-
-function nowServing(arr) {
-  if (arr.length === 0) {
-    return 'There is nobody waiting to be served!';
-  } else {
-    return arr[0]
-  }
->>>>>>> fbb2514b7238dbe5784e8868f8be65c464b64faf
 }
